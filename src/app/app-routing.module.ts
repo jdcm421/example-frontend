f0cb14166470741ff1a0f5527585433b5e89f3b7
Juan Diego Caceres Munares <44043720+juandiego436@users.noreply.github.com>
2022-09-13 17:32:22 -0500

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { ClienteComponent } from './cliente/cliente.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegistroComponent } from './registro/registro.component';
 
 const routes: Routes = [
